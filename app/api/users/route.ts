4ec7693aec918ff8773b8f24e6d22e21081b69e2
samoffem <samoffem@gmail.com>
2024-12-28 23:30:20 +0100

    creating api routes

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,5 +1,78 @@
+import connect from "@/lib/db"
+import User from "@/lib/models/users"
+import { Types } from "mongoose"
 import { NextResponse } from "next/server"
 
-export const GET = ()=>{
-    return new NextResponse("This is my first api")
+const ObjectId = require("mongoose").Types.ObjectId
+
+export const GET = async ()=>{
+    try {
+        await connect()
+        const users = await User.find()
+        return new NextResponse(JSON.stringify(users), {status: 200})
+    } catch (error: any) {
+        return new NextResponse(`Error in fetching users ${error.message}`, {status: 500})
+    }
+   
+}
+
+export const POST = async (request: Request)=>{
+    try {
+        const body = await request.json()
+        await connect()
+        const newUSer =  new User(body)
+        await newUSer.save();
+
+        return new NextResponse(JSON.stringify({message: "User created", user: newUSer }), {status: 201})
+    } catch (error: any) {
+        return new NextResponse(`Error in creating user ${error.message}`, {
+            status: 500
+        })
+    }
+}
+
+export const PATCH = async (request: Request)=>{
+    try {
+        const body = await request.json()
+        const {userId, newUsername} = body
+
+        await connect()
+
+        if(!userId || !newUsername){
+            return new NextResponse(
+                JSON.stringify({message: "ID and username is required"}),
+                {status: 400}
+            )
+        }
+
+        if(!Types.ObjectId.isValid(userId)){
+            return new NextResponse(
+                JSON.stringify({message: "Invalid user id"}),
+                {status: 400}
+            )
+        }
+
+        const updatedUser = await User.findOneAndUpdate(
+            {_id: new ObjectId(userId)},
+            {username: newUsername},
+            {new: true}
+        )
+
+        if(!updatedUser){
+            return new NextResponse(
+                JSON.stringify({message: "User not found in the database"}),
+                {status: 404}
+            )
+        }
+
+        return new NextResponse(
+            JSON.stringify({message: "User is updated", user: updatedUser}),
+            {status: 200}
+        )
+    } catch (error: any) {
+        return new NextResponse(
+            JSON.stringify({message: `Error updating user ${error.message}`}),
+            {status: 500}
+        )
+    }
 }
