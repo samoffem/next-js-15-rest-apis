1516c061b4f24ff6f2508092a70ebb63ca8b8d74
samoffem <samoffem@gmail.com>
2024-12-30 12:06:32 +0100

    delete request implementation

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -76,3 +76,47 @@ export const PATCH = async (request: Request)=>{
         )
     }
 }
+
+export const DELETE = async (request: Request)=>{
+    try {
+        const {searchParams} = new URL(request.url)
+        const userId = searchParams.get("userId")
+
+        if(!userId){
+            return new NextResponse(
+                JSON.stringify({message: "userId is required"}),
+                {status: 400}
+            )
+        }
+
+        if(!Types.ObjectId.isValid(userId)){
+            return new NextResponse(
+                JSON.stringify({message: "Invalid user id"}),
+                {status: 400}
+            )
+        }
+
+        await connect()
+
+        const deletedUser = await User.findByIdAndDelete(new Types.ObjectId(userId))
+
+        if(!deletedUser){
+            return new NextResponse(
+                JSON.stringify({message: "User not found in the database"}),
+                {status: 404}
+            )
+        }
+
+        return new NextResponse(
+            JSON.stringify({message: "User is deleted", user: deletedUser}),
+            {status: 200}
+        )
+
+
+    } catch (error: any) {
+        return new NextResponse(
+            JSON.stringify({message: `Error deleting user ${error.message}`}),
+            {status: 500}
+        )
+    }
+}
